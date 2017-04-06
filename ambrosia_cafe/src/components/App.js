822d88df637533499d8926968f8cc17b574dbf8e
JonCass <JCass1980@gmail.com>
2017-04-06 10:52:42 -0700

    basic accordion functionality

--- ambrosia_cafe/src/components/App.js
+++ ambrosia_cafe/src/components/App.js
@@ -8,7 +8,8 @@ const styles = {
 		height: 300,
 		width: 1440,
 		display: 'flex',
-		justifyContent: 'space-between'
+		justifyContent: 'space-between',
+		padding: 0
 	},
 	leftPhotos:{
 		height:300,
@@ -24,27 +25,37 @@ const styles = {
 	},
 	storyText:{
 		fontSize: 18,
-		padding: '0px 40px 0px 40px'
+		padding: '0px 40px 0px 40px',
+		display: 'inline-block'
 	}
 }
 class App extends React.Component {
-  render() {
-    return (
-      <div  style={styles.acc}>
-      	<div className='accSection'> Story
-      	</div>
-      		<div style={styles.story}>
-      			<div style={styles.leftPhotos}></div>
-      			<p style={styles.storyText}>Block of Text from API for Story |  Block of Text from API for Story </p>
-      			<div style={styles.rightPhotos}></div>
+  	handleAcc = (e) => {
+  		console.log(this.refs.storyOpener.className)
+  		if (this.refs.storyOpener.className.indexOf("w3-show") === -1){
+			this.refs.storyOpener.className += " w3-show"
+		}
+		else{
+			this.refs.storyOpener.className = this.refs.storyOpener.className.replace (" w3-show", "")
+			console.log(this.refs.storyOpener.className)
+		}
+	}
+  	render() {
+    	return (
+      		<div  style={styles.acc}>
+      			<button type="button" className='accSection' onClick={this.handleAcc} value={"storyOpener"}> Story</button>
+      			<div style={styles.story} className="w3-hide w3-btn w3-block w3-black w3-left-align" ref="storyOpener">
+      				<div style={styles.leftPhotos}></div>
+      				<p style={styles.storyText}>Block of Text from API for Story |  Block of Text from API for Story </p>
+      				<div style={styles.rightPhotos}></div>
+      			</div>
+      			<div className='accSection' style={styles.menu}> Menu
+      			</div>
+      			<div className='accSection' style={styles.reservations}> Reservations
+      			</div>
       		</div>
-      	<div className='accSection' style={styles.menu}> Menu
-      	</div>
-      	<div className='accSection' style={styles.reservations}> Reservations
-      	</div>
-      </div>
-    )
-  }
+    	)
+  	}
 }
 
 export default App

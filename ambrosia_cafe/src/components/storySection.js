3e8ea9f6b55feec1f527bbd5901579d914b74c70
JonCass <JCass1980@gmail.com>
2017-04-07 13:30:02 -0700

    story in place styling now

--- ambrosia_cafe/src/components/storySection.js
+++ ambrosia_cafe/src/components/storySection.js
@@ -3,21 +3,14 @@ import store from '../store'
 import {getMenu} from '../api/menu'
 
 const styles = { 
-  menuCategory:{
-    fontSize: 22,
-    fontFamily: 'Rock Salt',
-    border: 'solid black 2px',
-    width: '100%',
-    display: 'inline-block',
-    borderWidth: '0 0 2px 0',
-    paddingBottom: 4,
-    textAlign: 'left'
-  },
   storyText:{
   	width: 840,
     height: 300,
     display:'inline-block',
     padding:'20px 25px 10px 25px',
+    textOverflow: 'initial',
+    whiteSpace: 'normal',
+    overflow: 'auto',
   }
 }
 
@@ -29,9 +22,8 @@ class StorySection extends React.Component {
   componentWillMount() {
       this.unsubscribe = store.subscribe(()=>{
           const appState = store.getState().menuReducer.menu.story.text
-    
       this.setState({
-            menu: appState
+            story: appState
           })
       })
       getMenu()

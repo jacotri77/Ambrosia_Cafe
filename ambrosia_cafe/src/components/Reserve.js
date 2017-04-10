28ef1fe1e09e9f481add9c85d335de165ba7eff9
JonCass <JCass1980@gmail.com>
2017-04-10 00:49:54 -0700

    Redux boooo

--- ambrosia_cafe/src/components/Reserve.js
+++ ambrosia_cafe/src/components/Reserve.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import db from '../../db.json'
+import store from '../store'
+import {getMenu} from '../api/menu'
 
 const styles = {
   container:{
-    height: 640,
+    height: 660,
     width: 840,
     padding : ' 20px 0 0 40px'
   },
@@ -51,39 +54,75 @@ const styles = {
   	borderRadius: '5px',
   	fontFamily: 'Questrial',
   	marginLeft: 20
+  },
+  verifyOn:{
+  	fontFamily: 'Questrial',
+  	color: 'black',
+  	float: 'left',
+  	clear: 'right',
+  	margin: 10,
+  	fontSize: 20,
+  	border: 'solid 2px black',
+  	padding: 5
+  },
+  verifyOff:{
+  	display: 'none'
   }
 }
 
 class Reserve extends React.Component {
   constructor(props) {
     super(props)
-    this.state = { name : '', guests : '', date: '', notes : '', seat : 'Indoor'}
+    this.state = { displayBox : false, name : '', guests : '', date: '', notes : '', seat : 'MainFloor', reservations: {}}
   }
+  componentWillMount() {
+      this.unsubscribe = store.subscribe(()=>{
+          const appState = store.getState().menuReducer.res
+      this.setState({
+            reservations : appState
+          })
+      })
+      getMenu()
+    }
+    componentWillUnmount() {
+      this.unsubscribe()
+    }
   handleName = (e) => {
     this.setState({
-      name: e.target.value
+      name: e.target.value,
+      displayBox: false
     })
   }
   handleGuests = (e) => {
     this.setState({
-      guests: e.target.value
+      guests: e.target.value,
+      displayBox: false
     })
   }
   handleDate = (e) => {
     this.setState({
-      date: e.target.value
+      date: e.target.value,
+      displayBox: false
     })
   }
   handleNotes = (e) => {
     this.setState({
-      notes: e.target.value
+      notes: e.target.value,
+      displayBox: false
     })
   }
   handleSeat = (e) => {
     this.setState({
-      seat: e.target.value
+      seat: e.target.value,
+      displayBox: false
     })
   }
+  handleSubmit = (e) => {
+	this.setState({
+        displayBox: true
+   	}) 
+  }
+
   render() {
     return (
       <div style={styles.container}>
@@ -97,13 +136,15 @@ class Reserve extends React.Component {
         	<label htmlFor="notes" style={styles.label}>Special Notes </label>
         	<textarea id="notes" onChange={this.handleNotes} value={this.state.notes}style={styles.inputBox}></textarea>
         	<label htmlFor="seatPref" style={styles.label}>Seating Preference </label>
-        	<select style={styles.select}>
-				<option value="Indoor">Indoor</option>
+        	<select onChange={this.handleSeat} style={styles.select}>
+				<option value="Indoor">Main Floor</option>
 				<option value="Veranda">Veranda</option>
-				<option value="Roof">Roof</option>
+				<option value="Balcony">Balcony</option>
+				<option value="Chaos Dimension">Chaos Dimension</option>
 			</select>
         </form>
         <button onClick={this.handleSubmit} style={styles.submit}> Submit Reservation </button>
+       	<p style={this.state.displayBox === true ? styles.verifyOn : styles.verifyOff }> Thank you {this.state.name} your table for {this.state.guests} guests is confirmed on the date of {this.state.date} in the [ {this.state.seat} ] section.</p> 	
       </div>
     )
   }

119d5064f203c9db1be4c7ca26fb1130c83ad8fc
JonCass <JCass1980@gmail.com>
2017-04-09 23:36:03 -0700

    form complete wiring in progress

--- ambrosia_cafe/src/components/Reserve.js
+++ ambrosia_cafe/src/components/Reserve.js
@@ -41,26 +41,61 @@ const styles = {
   	fontSize: 18,
   	height: 35,
   	width: 200
+  },
+  submit:{
+  	height: 60,
+  	width: 230,
+  	fontSize: 24,
+  	float: 'left',
+  	background: '#6CB519',
+  	borderRadius: '5px',
+  	fontFamily: 'Questrial',
+  	marginLeft: 20
   }
 }
 
 class Reserve extends React.Component {
   constructor(props) {
     super(props)
+    this.state = { name : '', guests : '', date: '', notes : '', seat : 'Indoor'}
   }
-
+  handleName = (e) => {
+    this.setState({
+      name: e.target.value
+    })
+  }
+  handleGuests = (e) => {
+    this.setState({
+      guests: e.target.value
+    })
+  }
+  handleDate = (e) => {
+    this.setState({
+      date: e.target.value
+    })
+  }
+  handleNotes = (e) => {
+    this.setState({
+      notes: e.target.value
+    })
+  }
+  handleSeat = (e) => {
+    this.setState({
+      seat: e.target.value
+    })
+  }
   render() {
     return (
       <div style={styles.container}>
       	<form>
         	<label htmlFor="fullName" style={styles.label}>Full Name </label>
-        	<input type="text" id="fullName" style={styles.inputLine} />
+        	<input type="text" onChange={this.handleName} value={this.state.name} id="fullName" style={styles.inputLine} />
         	<label htmlFor="numGuests" style={styles.label}>Number of Guests </label>
-        	<input type="text" id="numGuests" style={styles.inputLine} />
+        	<input type="text" onChange={this.handleGuests} value={this.state.guests} id="numGuests" style={styles.inputLine} />
         	<label htmlFor="date" style={styles.label}>Date </label>
-        	<input type="text" id="date" style={styles.inputLine} />
+        	<input type="text" onChange={this.handleDate} value={this.state.date} id="date" style={styles.inputLine} />
         	<label htmlFor="notes" style={styles.label}>Special Notes </label>
-        	<textarea id="notes" style={styles.inputBox}></textarea>
+        	<textarea id="notes" onChange={this.handleNotes} value={this.state.notes}style={styles.inputBox}></textarea>
         	<label htmlFor="seatPref" style={styles.label}>Seating Preference </label>
         	<select style={styles.select}>
 				<option value="Indoor">Indoor</option>
@@ -68,6 +103,7 @@ class Reserve extends React.Component {
 				<option value="Roof">Roof</option>
 			</select>
         </form>
+        <button onClick={this.handleSubmit} style={styles.submit}> Submit Reservation </button>
       </div>
     )
   }
